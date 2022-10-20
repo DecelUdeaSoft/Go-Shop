12b9a8b62147bcfd52f15303ee14cb2c685cb746
Carolina <108636087+karobm@users.noreply.github.com>
2022-10-20 11:09:57 -0500

    Enrutado basico para productos

--- back/server.js
+++ back/server.js
@@ -4,6 +4,7 @@ const app=require("./app")
 const dotenv=require("dotenv");
 dotenv.config({path: 'back/config/config.env'})
 
+//llamemos al server
 const server=app.listen(process.env.PORT, () => {
     
     console.log(`Servidor iniciando en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
