ebc49f9d92091245d4e900d3b68c3d8bc86a3762
Carolina <108636087+karobm@users.noreply.github.com>
2022-10-20 22:02:59 -0500

    se crea models

--- back/server.js
+++ back/server.js
@@ -1,11 +1,14 @@
 const app=require("./app")
+const connectDatabase = require("./config/database");
 
 //Setear el archivo de configuracion
 const dotenv=require("dotenv");
 dotenv.config({path: 'back/config/config.env'})
 
+//configurar base de datos
+connectDatabase();
+
 //llamemos al server
 const server=app.listen(process.env.PORT, () => {
-    
-    console.log(`Servidor iniciando en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+        console.log(`Servidor iniciando en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
 })
