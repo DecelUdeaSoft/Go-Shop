9596cb6b52f2182a52d1b7c80eca294b5c71ac00
Carolina <108636087+karobm@users.noreply.github.com>
2022-10-26 13:29:37 -0500

    Se crea Frontend
    Se sube logo, nombre de tienda, se realizan instalaciones, inicio de de pagina localhost

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Tienda de Relojes Deportivos Go Shop</h1>
+        <img src="./images/logo Go-Shopvsc.png" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
