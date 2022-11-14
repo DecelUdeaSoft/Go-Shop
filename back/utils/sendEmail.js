c8c724e6138dd5184adf96a0b8a5a57288ec4e3b
karolina <108636087+karobm@users.noreply.github.com>
2022-11-14 12:44:51 -0500

    actualizar informacion de usuarios

--- back/utils/sendEmail.js
+++ back/utils/sendEmail.js
@@ -2,15 +2,15 @@ const nodemailer= require("nodemailer")
 
 const sendEmail = async options =>{
     const transport = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-          user: "0a62c4c5eaef39",
-          pass: "014e13b634285e"
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "0a62c4c5eaef39",
+        pass: "014e13b634285e"
         }
       });
     const mensaje={
-        from: "GoShop Store <noreply@goshop.com>",
+        from: "GoShop Store <norepli@goshop.com>",
         to: options.email,
         subject: options.subject,
         text: options.mensaje
