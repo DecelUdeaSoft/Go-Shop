9e4d4fae3a6f0275fb55d6cd6d1380daeff2c014
karolina <108636087+karobm@users.noreply.github.com>
2022-11-16 11:57:58 -0500

    se crea paginacion en front
    cambios en reducer, actions y home

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,12 +1,14 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import MetaData from './layout/MetaData'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../actions/productActions'
 import { Link } from 'react-router-dom'
 import { useAlert } from 'react-alert'
+import Pagination from 'react-js-pagination'
 
 export const Home = () => {
-    const { loading, productos, error} = useSelector(state=> state.products)
+    const[currentPage, setCurrentPage] = useState(1)
+    const { loading, products, error, resPerPage, productsCount} = useSelector(state=> state.products)
     const alert= useAlert();
 
     const dispatch = useDispatch();
@@ -15,8 +17,11 @@ export const Home = () => {
             return alert.error(error)
         }
 
-        dispatch(getProducts());
-    }, [dispatch])
+        dispatch(getProducts(currentPage));
+    }, [dispatch, alert, error, currentPage])
+    function setCurrentPageNo(pageNumber){
+        setCurrentPage(pageNumber)
+    }
 
 
     return (
@@ -28,29 +33,43 @@ export const Home = () => {
 
             <section id="productos" className='container mt-5'>
                 <div className='row'>
-                {console.log(productos)}
-                    {productos && productos.map (producto => (
+                    {products && products.map (producto => (
                         <div key={producto._id} className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src={producto.imagen1[0].url} alt={producto.imagen1[0].public_id}></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><Link to={`/producto/${producto._id}`}>{producto.nombre}</Link></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner' style={{width: `${(producto.calificacion/5)*100}%`}}></div>
+                            <div className='card p-3 rounded'>
+                                <img className='card-img-top mx-auto' src={producto.imagen1[0].url} alt={producto.imagen1[0].public_id}></img>
+                                <div className='card-body d-flex flex-column'>
+                                    <h5 id="titulo_producto"><Link to={`/producto/${producto._id}`}>{producto.nombre}</Link></h5>
+                                    <div className='rating mt-auto'>
+                                        <div className='rating-outer'>
+                                            <div className='rating-inner' style={{width: `${(producto.calificacion/5)*100}%`}}></div>
+                                        </div>
+                                        <span id="No_de_opiniones"> {producto.numCalificaciones} Reviews</span>
                                     </div>
-                                    <span id="No_de_opiniones"> {producto.numCalificaciones} Reviews</span>
+                                    <p className='card-text'>${producto.precio}</p><Link to={`/producto/${producto._id}`} id="view_btn" className='btn btn-block'>
+                                        Ver detalle
+                                    </Link>
                                 </div>
-                                <p className='card-text'>${producto.precio}</p><Link to={`/producto/${producto._id}`} id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </Link>
                             </div>
                         </div>
-                    </div>
 
                     ))}
-                    </div>
+                </div>
             </section>
+
+            <div className='d-flex justify-content-center mt-5'>
+                <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNo}
+                nextPageText={'Siguiente'}
+                prevPageText={'Anterior'}
+                firstPageText={'Primera'}
+                lastPageText={'Ultima'}
+                itemClass='page-item'
+                linkClass='page-link'
+                        />        
+            </div>
 
                 </Fragment>
 
