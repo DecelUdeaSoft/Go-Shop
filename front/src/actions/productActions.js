9048f15a8f3d52cfb90c82b902e2306e1452b0f1
karolina <108636087+karobm@users.noreply.github.com>
2022-11-17 12:11:16 -0500

    todo lo relacionado con el front
    clases nov 8 y 9 de natali

--- front/src/actions/productActions.js
+++ front/src/actions/productActions.js
@@ -10,13 +10,15 @@ import {
     CLEAR_ERRORS
 } from '../constants/productConstants';
 
-export const getProducts = (currentPage =1) => async(dispatch)=>{
+export const getProducts = ( currentPage =1, keyword='', precio) => async(dispatch)=>{
     try {
         dispatch({type: ALL_PRODUCTS_REQUEST})
 
-        const {data} = await axios.get(`api/productos?page=${currentPage}`)
+        let link = `/api/productos?keyword=${keyword}&page=${currentPage}&precio[gte]=${precio[0]}&precio[lte]=${precio[1]}`
 
+        const { data } = await axios.get(link)
 
+        
         dispatch({
             type:ALL_PRODUCTS_SUCCESS,
             payload: data
--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -2,79 +2,109 @@ import React, { Fragment, useEffect, useState } from 'react'
 import MetaData from './layout/MetaData'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../actions/productActions'
-import { Link } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { useAlert } from 'react-alert'
 import Pagination from 'react-js-pagination'
+import Slider from "rc-slider"
+import 'rc-slider/assets/index.css'
+
 
 export const Home = () => {
-    const[currentPage, setCurrentPage] = useState(1)
-    const { loading, products, error, resPerPage, productsCount} = useSelector(state=> state.products)
-    const alert= useAlert();
+    const params = useParams();
+    const Keyword = params.Keyword;
+    const [precio, setPrecio] = useState([1000, 100000])
+    const [currentPage, setCurrentPage] = useState(1)
+    const { loading, products, error, resPerPage, productsCount } = useSelector(state => state.products)
+    const alert = useAlert();
+
 
     const dispatch = useDispatch();
     useEffect(() => {
-        if (error){
+        if (error) {
             return alert.error(error)
         }
 
-        dispatch(getProducts(currentPage));
-    }, [dispatch, alert, error, currentPage])
-    function setCurrentPageNo(pageNumber){
+        dispatch(getProducts(currentPage, Keyword, precio));
+    }, [dispatch, alert, error, currentPage, Keyword, precio])
+
+    function setCurrentPageNo(pageNumber) {
         setCurrentPage(pageNumber)
     }
 
 
     return (
         <Fragment>
-            {loading ? <i className="fa fa-refresh fa-spin fa-3x fa-fw"></i> :(
+            {loading ? <i className="fa fa-refresh fa-spin fa-3x fa-fw"></i> : (
                 <Fragment>
                     <MetaData title="Los mejores Relojes deportivos"></MetaData>
-            <h1 id="encabezado_productos">Relojes Deportivos</h1>
+                    <h1 id="encabezado_productos">Relojes Deportivos</h1>
 
-            <section id="productos" className='container mt-5'>
-                <div className='row'>
-                    {products && products.map (producto => (
-                        <div key={producto._id} className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                            <div className='card p-3 rounded'>
-                                <img className='card-img-top mx-auto' src={producto.imagen1[0].url} alt={producto.imagen1[0].public_id}></img>
-                                <div className='card-body d-flex flex-column'>
-                                    <h5 id="titulo_producto"><Link to={`/producto/${producto._id}`}>{producto.nombre}</Link></h5>
-                                    <div className='rating mt-auto'>
-                                        <div className='rating-outer'>
-                                            <div className='rating-inner' style={{width: `${(producto.calificacion/5)*100}%`}}></div>
+                    <section id="productos" className='container mt-5'>
+                        <div className='row'>
+                            <Slider
+                                range
+                                className='t-slider'
+                                marks={{
+                                    1000: `$1000`,
+                                    100000: `$100000`
+                                }}
+                                min={1000}
+                                max={100000}
+                                defaultValue={[1000, 100000]}
+                                tipFormatter={value => `$${value}`}
+                                tipProps={{
+                                    placement: 'top',
+                                    prefixCls: 'rc-slider-tooltip',
+                                    visible: true
+                                }}
+                                value={precio}
+                                onChange={precio => setPrecio(precio)}
+
+                            ></Slider>
+
+
+                            {products && products.map(producto => (
+                                <div key={producto._id} className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                                    <div className='card p-3 rounded'>
+                                        <img className='card-img-top mx-auto' src={producto.imagen1[0].url} alt={producto.imagen1[0].public_id}></img>
+                                        <div className='card-body d-flex flex-column'>
+                                            <h5 id="titulo_producto"><Link to={`/producto/${producto._id}`}>{producto.nombre}</Link></h5>
+                                            <div className='rating mt-auto'>
+                                                <div className='rating-outer'>
+                                                    <div className='rating-inner' style={{ width: `${(producto.calificacion / 5) * 100}%` }}></div>
+                                                </div>
+                                                <span id="No_de_opiniones"> {producto.numCalificaciones} Reviews</span>
+                                            </div>
+                                            <p className='card-text'>${producto.precio}</p><Link to={`/producto/${producto._id}`} id="view_btn" className='btn btn-block'>
+                                                Ver detalle
+                                            </Link>
                                         </div>
-                                        <span id="No_de_opiniones"> {producto.numCalificaciones} Reviews</span>
                                     </div>
-                                    <p className='card-text'>${producto.precio}</p><Link to={`/producto/${producto._id}`} id="view_btn" className='btn btn-block'>
-                                        Ver detalle
-                                    </Link>
                                 </div>
-                            </div>
+
+                            ))}
                         </div>
+                    </section>
 
-                    ))}
-                </div>
-            </section>
-
-            <div className='d-flex justify-content-center mt-5'>
-                <Pagination
-                activePage={currentPage}
-                itemsCountPerPage={resPerPage}
-                totalItemsCount={productsCount}
-                onChange={setCurrentPageNo}
-                nextPageText={'Siguiente'}
-                prevPageText={'Anterior'}
-                firstPageText={'Primera'}
-                lastPageText={'Ultima'}
-                itemClass='page-item'
-                linkClass='page-link'
-                        />        
-            </div>
+                    <div className='d-flex justify-content-center mt-5'>
+                        <Pagination
+                            activePage={currentPage}
+                            itemsCountPerPage={resPerPage}
+                            totalItemsCount={productsCount}
+                            onChange={setCurrentPageNo}
+                            nextPageText={'Siguiente'}
+                            prevPageText={'Anterior'}
+                            firstPageText={'Primera'}
+                            lastPageText={'Ultima'}
+                            itemClass='page-item'
+                            linkClass='page-link'
+                        />
+                    </div>
 
                 </Fragment>
 
             )}
-            
+
 
         </Fragment>
     )
