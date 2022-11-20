a696a14c0e1fa2df62b9103b6c07fb310d57d1e5
karolina <108636087+karobm@users.noreply.github.com>
2022-11-20 13:43:31 -0500

    clases y videos de nov 11
    se crea proteccion del dashboars para que solo lo vea admon. Usuario puede registrarse y ver solo productos

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './components/layout/Header';
 import { Footer } from './components/layout/Footer';
 import Home from './components/Home';
 import { ProductDetails } from './components/products/ProductDetails';
+
 //Router traido desde react-router-dom (no confundir con el de express)
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/admin/Dashboard';
@@ -11,9 +12,17 @@ import ProductsList from './components/admin/ProductsList';
 import NewProduct from './components/admin/newProduct';
 import Cart from './components/cart/Cart';
 import { Login } from './components/user/Login';
-
+import { Register } from './components/user/Register';
+import { loadUser } from './actions/userActions';
+import store from "./store"
+import { Profile } from './components/user/Profile';
+import ProtectedRoute from './routes/ProtectedRoute';
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+    
   return (
     <Router>
     <div className="App">
@@ -23,13 +32,17 @@ function App() {
             <Route path="/" element={<Home />}/>
             <Route path="/Home" element={<Home />}/>
             <Route path="/producto/:id" element={<ProductDetails />}/>
-            <Route path="/dashboard" element={<Dashboard />}/>
             <Route path="/productList" element={<ProductsList />}/>
             <Route path="/nuevoProducto" element={<NewProduct />}/>
             <Route path="/search/:keyword" element={<Home />}/>   
             <Route path="/carrito" element={<Cart />}/>
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element= {<Register />} />
+            <Route path="/yo" element={<Profile />} />
 
+            {/*Ruta protegida*/}
+            <Route path="/dashboard"
+              element={<ProtectedRoute isAdmin={true}><Dashboard /></ProtectedRoute>} />
           </Routes>
         </div>
         <Footer />
--- front/src/actions/userActions.js
+++ front/src/actions/userActions.js
@@ -5,6 +5,15 @@ import {
     LOGIN_SUCCESS,
     LOGIN_FAIL,
     CLEAR_ERRORS,
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_FAIL,
+    LOAD_USER_REQUEST,
+    LOAD_USER_SUCCESS,
+    LOAD_USER_FAIL,
+    LOGOUT_SUCCESS,
+    LOGOUT_FAIL,
+
 } from "../constants/userConstants"
 
 //Login
@@ -27,6 +36,62 @@ export const login = (email, password) => async (dispatch) => {
     catch (error) { 
         dispatch({
             type:LOGIN_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+//REGISTRAR USUARIO
+export const register = (userData) => async (dispatch) => {
+    try {
+        dispatch({ type: REGISTER_USER_REQUEST })
+
+        const config={
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }
+        const {data} = await axios.post('/api/usuario/registro', userData, config)
+
+        dispatch({
+            type: REGISTER_USER_SUCCESS,
+            payload: data.user
+        })
+    }
+    catch (error) { 
+        dispatch({
+            type: REGISTER_USER_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+//CARGAR EL USUARIO (LOAD USER)
+export const loadUser=()=> async(dispatch) =>{
+    try{
+        dispatch({type: LOAD_USER_REQUEST})
+        const {data} = await axios.get("/api/yo")
+        dispatch({
+            type: LOAD_USER_SUCCESS,
+            payload: data.user
+        })
+    }catch(error){
+        dispatch({
+            type: LOAD_USER_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+//logout User
+export const logout = () => async (dispatch)=>{
+    try{
+        await axios.get("/api/logout")
+        dispatch({
+            type: LOGOUT_SUCCESS,
+        })
+    } catch(error){
+        dispatch({
+            type:LOGOUT_FAIL,
             payload: error.response.data.message
         })
     }
